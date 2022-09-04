b2be451f0935a898d0dfea2cabaa2b01a6d8983d
Violet <kmuu011@naver.com>
2022-09-05 00:02:29 +0900

    feat: orRegExp 만드는 모듈 구현, GNB 조건부 렌더링 로직 리팩토링 css: GNB 하단 구분선 적용

--- src/component/common/GlobalNavigation.tsx
+++ src/component/common/GlobalNavigation.tsx
@@ -1,16 +1,21 @@
 import styles from '../../../styles/common/GlobalNavigation.module.scss';
-import {FunctionComponent, useEffect, useRef, useState} from "react";
+import {FunctionComponent, useEffect, useState} from "react";
 import Link from "next/link";
-import {AxiosResponse} from "axios";
 import menuButton from "../../../public/static/button/menu/menu.svg"
 import Image from "next/image";
+import {orRegExpMaker} from "../../utils/utils";
+
+const disabledLocationList: string[] = [
+    '/', 'signUp'
+];
 
 const GlobalNavigation: FunctionComponent = () => {
     const [showNavi, setShowNavi] = useState(false);
 
     useEffect(() => {
         const pathName: string = window.location.pathname;
-        setShowNavi(!(/^\/$|^signUp$/).test(pathName));
+
+        setShowNavi(!orRegExpMaker(disabledLocationList).test(pathName));
     }, []);
 
     return showNavi ? <div className={styles.container}>
