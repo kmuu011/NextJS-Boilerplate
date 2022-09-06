84a31fc5b3e86580007f7de816dda4926297d33d
Violet <kmuu011@naver.com>
2022-09-07 00:45:44 +0900

    feat: 리렌더링 될때마다 atom key duplicate 오류를 해결하기 위해 랜덤값 생성기 구현 및 적용

--- src/const/function.ts
+++ src/const/function.ts
@@ -1,5 +1,24 @@
 import {SetterOrUpdater} from "recoil";
+const dummyStr: string = 'QWERTYUIOPASDFGHJKLZXCVBNM0123456789';
 
 export const hideSideMenuBar = (setShowSideBar: SetterOrUpdater<boolean>) => {
     setShowSideBar(false);
 }
+
+export const createKey = (count?: number, time?: boolean) => {
+    count = count || 20;
+
+    let key = '';
+
+    for (let i=0; i<count; i++) {
+        const ran_int = Math.floor(Math.random() * dummyStr.length);
+
+        key += dummyStr[ran_int];
+    }
+
+    if(time){
+        key += '_' + Date.now();
+    }
+
+    return key;
+};
