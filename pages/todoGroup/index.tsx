f21e5b97ec5f27972ac9dd66f24f5a4d7005c0f4
Violet <kmuu011@naver.com>
2022-09-09 23:15:11 +0900

    feat: todoGroup list보기시 infinity스크롤 적용

--- pages/todoGroup/index.tsx
+++ pages/todoGroup/index.tsx
@@ -8,10 +8,13 @@ import addImage from "../../public/static/button/add/add.svg";
 import {createTodoGroupApi, selectTodoGroupApi} from "../../src/api/todoGroup";
 import TodoGroupItem from "../../src/component/todoGroup/todoGroupItem";
 import {TodoGroupItemType} from "../../src/type/todoGroup";
+import InfiniteScroll from 'react-infinite-scroller';
 
 const Home: NextPage = () => {
     const [todoGroupList, setTodoGroupList] = useState<TodoGroupItemType[]>([]);
     const [totalCount, setTotalCount] = useState(0);
+    const [page, setPage] = useState(1);
+    const [last, setLast] = useState(0)
 
     const createTodoGroup = async (): Promise<void> => {
         const response = await createTodoGroupApi({title: `할일 그룹 ${totalCount + 1}`});
@@ -24,8 +27,12 @@ const Home: NextPage = () => {
         await getTodoGroup()
     }
 
-    const getTodoGroup = async (): Promise<void> => {
-        const response = await selectTodoGroupApi({page: 1, count: 12});
+    const getTodoGroup = async (nextPage?: boolean): Promise<void> => {
+        const selectPage = nextPage ? page + 1 : page;
+
+        if (last !== 0 && last < selectPage) return;
+
+        const response = await selectTodoGroupApi({page: selectPage, count: 12});
 
         if (response?.status !== 200) {
             alert(response?.data.message);
@@ -33,7 +40,9 @@ const Home: NextPage = () => {
         }
 
         setTotalCount(response.data.totalCount);
-        setTodoGroupList(response.data.items);
+        setTodoGroupList([...todoGroupList, ...response.data.items]);
+        setPage(response.data.page);
+        setLast(response.data.last);
     }
 
     const circleButtonProps: CircleButtonProps = {
@@ -53,7 +62,13 @@ const Home: NextPage = () => {
                 <CircleButton {...circleButtonProps}/>
             </div>
 
-            <div className={styles.todoGroupListWarp}>
+            <InfiniteScroll
+                className={styles.todoGroupListWarp}
+                initialLoad={false}
+                pageStart={1}
+                loadMore={() => getTodoGroup(true)}
+                hasMore={true}
+            >
                 {
                     todoGroupList.map((todoGroup, i) => {
                         return <TodoGroupItem
@@ -63,7 +78,8 @@ const Home: NextPage = () => {
                         />
                     })
                 }
-            </div>
+            </InfiniteScroll>
+
         </div>
     )
 }
