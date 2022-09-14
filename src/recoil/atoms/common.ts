5828323c411c8e7eff8250e12a3af5f71fed47e1
Violet <kmuu011@naver.com>
2022-09-14 22:28:36 +0900

    recoil의 atom 키 처리 부분 변경

--- src/recoil/atoms/common.ts
+++ src/recoil/atoms/common.ts
@@ -1,8 +1,12 @@
 import {atom} from "recoil";
 import {createKey} from "../../const/function";
 
+const getKey = (key: string) => {
+    return key + createKey();
+}
+
 export const showSideBarAtom = atom({
-    key: 'showSideBar' + createKey(),
+    key: getKey('showSideBar'),
     default: false
 });
 
